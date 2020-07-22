2aebb477509de471db60c1f5785cf77a65523805
Leandro Muto <leandro.muto@gmail.com>
2020-07-22 20:55:35 -0300

    move logger file to utils folder

--- test/mocha/helper/logger.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment')
-
-const now = () => moment().utc().format('D/MM/YY HH:mm:ss:SSSS')
-
-module.exports = {
-  logger: (text) => {
-    logFile.write(`${now()} - ${text}\n`)
-  }
-}
